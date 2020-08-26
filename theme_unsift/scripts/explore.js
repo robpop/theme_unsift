bad1654f48df6a2f3c95be3c4b0a4e85684ec279
Robert F. Popeleski <therfp97@gmail.com>
2020-08-25 23:02:09 -0400

    Mobile explore

--- theme_unsift/scripts/explore.js
+++ theme_unsift/scripts/explore.js
@@ -21,6 +21,12 @@ $(() => {
     let star_half = "<svg width='1.2em' height='1.2em' viewBox='0 0 16 16' class='bi bi-star-half m-1' fill='#4285F4' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M5.354 5.119L7.538.792A.516.516 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.537.537 0 0 1 16 6.32a.55.55 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.519.519 0 0 1-.146.05c-.341.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.171-.403.59.59 0 0 1 .084-.302.513.513 0 0 1 .37-.245l4.898-.696zM8 12.027c.08 0 .16.018.232.056l3.686 1.894-.694-3.957a.564.564 0 0 1 .163-.505l2.906-2.77-4.052-.576a.525.525 0 0 1-.393-.288L8.002 2.223 8 2.226v9.8z'/></svg>";
     let star_empty = "<svg width='1.2em' height='1.2em' viewBox='0 0 16 16' class='bi bi-star m-1' fill='#4285F4' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z'/></svg>";
         
+    let sideBar = document.getElementById("sideBar");
+    let body = document.getElementsByTagName("body")[0];
+    let exploreMobileExpandSideBar = document.getElementById("exploreMobileExpandSideBar");
+    let hammertime_body = null;
+    let hammertime_sideBar = null
+    let sideBarOpen = null;
 
     // Fill product modal with corresponding information from clicked card
     let update_modal_information = function(e) {
@@ -81,5 +87,58 @@ $(() => {
         $("#actionModalReviewsDraft").val("");
         $("#actionModalReviewsDraft").blur();
     });
+
+
+    // Enable swipe events on side bar when mobile
+    if ($(".-mobile").css("display") !== "none") {
+        sideBarOpen = false;
+        hammertime_body = new Hammer(body);
+        hammertime_body.on('swiperight', function(ev) {
+            // open sideBar
+            $(sideBar).addClass("show")
+            $(sideBar).find(".col-lg-12").addClass("show");
+            $(sideBar).find(".list-group-item").addClass("show");
+            $(exploreMobileExpandSideBar).children().eq(0).addClass("d-none");
+            $(exploreMobileExpandSideBar).children().eq(1).removeClass("d-none");
+            $(exploreMobileExpandSideBar).addClass("close");
+            $(body).addClass("noscroll");
+            sideBarOpen = true;
+        });
+        hammertime_sideBar = new Hammer(sideBar);
+        hammertime_sideBar.on('swipeleft', function(ev) {
+            // close sideBar
+            $(sideBar).removeClass("show")
+            $(sideBar).find(".col-lg-12").removeClass("show");
+            $(sideBar).find(".list-group-item").removeClass("show");
+            $(exploreMobileExpandSideBar).children().eq(1).addClass("d-none");
+            $(exploreMobileExpandSideBar).children().eq(0).removeClass("d-none");
+            $(exploreMobileExpandSideBar).removeClass("close");
+            $(body).removeClass("noscroll");
+            sideBarOpen = false;
+        });
+
+        $(exploreMobileExpandSideBar).click(() => {
+            if (sideBarOpen) {
+                $(sideBar).removeClass("show")
+                $(sideBar).find(".col-lg-12").removeClass("show");
+                $(sideBar).find(".list-group-item").removeClass("show");
+                $(exploreMobileExpandSideBar).children().eq(1).addClass("d-none");
+                $(exploreMobileExpandSideBar).children().eq(0).removeClass("d-none");
+                $(exploreMobileExpandSideBar).removeClass("close");
+                $(body).removeClass("noscroll");
+                sideBarOpen = false;
+            } else {
+                $(sideBar).addClass("show")
+                $(sideBar).find(".col-lg-12").addClass("show");
+                $(sideBar).find(".list-group-item").addClass("show");
+                $(exploreMobileExpandSideBar).children().eq(0).addClass("d-none");
+                $(exploreMobileExpandSideBar).children().eq(1).removeClass("d-none");
+                $(exploreMobileExpandSideBar).addClass("close");
+                $(body).addClass("noscroll");
+                sideBarOpen = true;
+            }
+            
+        });
+    }
     
 });
