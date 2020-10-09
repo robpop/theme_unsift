c91a32c901e6faba5afbd4a9c5463db1de051faf
Robert F. Popeleski <therfp97@gmail.com>
2020-10-09 02:33:18 -0400

    CSS reset

--- reduced-js/support.php
+++ reduced-js/support.php
@@ -57,6 +57,7 @@ if (array_key_exists('email', $_POST)) {
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="apple-mobile-web-app-capable" content="yes">
 
     <title>Contact support - LogoIpsum</title>
 
