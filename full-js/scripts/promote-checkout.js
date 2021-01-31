54858aed430c461c7231b19a0323807e565d456c
Robert F. Popeleski <therfp97@gmail.com>
2021-01-30 21:27:38 -0500

    Advertise checkout page - budget selector

--- full-js/scripts/promote-checkout.js
+++ full-js/scripts/promote-checkout.js
@@ -1,27 +1,11 @@
 "use strict";
 
-function count_to(id, start, end, duration) {
-    console.log(id+" "+start+" "+end+" "+duration);
-    if (start === end) return;
-    var range = end - start;
-    var current = start;
-    var increment = end > start? 1 : -1;
-    var stepTime = Math.abs(Math.floor(duration / range));
-    var timer = setInterval(function() {
-        current += increment;
-        id.innerHTML = current;
-        if (current == end) {
-            clearInterval(timer);
-        }
-    }, stepTime);
-}
-
 $(() => {
     
     let advertiseBudgetInput = $("#advertiseBudgetInput");
     let advertiseBudgetSlider = $("#advertiseBudgetSlider");
-    let advertiseTotalCost = document.getElementById("advertiseTotalCost");
-    let advertiseTotalClicks = document.getElementById("advertiseTotalClicks");
+    let advertiseTotalCost = $("#advertiseTotalCost");
+    let advertiseTotalClicks = $("#advertiseTotalClicks");
 
     // These currency denominations are supported by default
     // You can add any denomination you need for your platform to the map below
@@ -38,10 +22,28 @@ $(() => {
         "INR": "\u20B9"
     };
 
-    let selected_denomination = denominations[advertiseBudgetInput.attr("data-denomination")];
-    let last_value = advertiseBudgetSlider.val();
+    // Use a maximum budget of $5,000 by default
+    // Change this to fit with your platform's needs
+    let maximum_budget = 5000;
+    let minimum_budget = 0;
 
-    let sync_input_slider = function(is_slider, value) {
+
+    let selected_denomination = denominations[advertiseBudgetInput.attr("data-denomination")];
+    let update_cost_clicks = null;
+
+    // This is the number of clicks a buyer gets per $1
+    // Change this to fit with your platform's needs
+    let clicks_unit_multiplier = 15;
+
+
+    String.prototype.removeCharAt = function (i) {
+        var tmp = this.split('');
+        tmp.splice(i - 1 , 1);
+        return tmp.join('');
+    }
+
+
+    let sync_input_slider = function(is_slider) {
         if (is_slider) {
             // sync input
             advertiseBudgetInput.val(selected_denomination+advertiseBudgetSlider.val());
@@ -49,14 +51,28 @@ $(() => {
             // sync slider
             advertiseBudgetSlider.val(advertiseBudgetInput.val().substr(1));
         }
-        count_to(advertiseTotalCost, parseInt(last_value), parseInt(advertiseBudgetSlider.val()), 250);
-        last_value = advertiseBudgetSlider.val();
+
+        window.clearTimeout(update_cost_clicks);
+        update_cost_clicks = setTimeout(() => {
+            advertiseTotalCost.parent().removeClass("fadeInUp delay-150ms");
+            advertiseTotalClicks.parent().removeClass("fadeInUp");
+            advertiseTotalCost.parent().addClass("fadeOutDown");
+            advertiseTotalClicks.parent().addClass("fadeOutDown delay-150ms");
+            setTimeout(() => {
+                advertiseTotalCost.text(numeral(parseInt(advertiseBudgetInput.val().substr(1))).format('0,0'));
+                advertiseTotalClicks.text(numeral(parseInt(advertiseBudgetInput.val().substr(1))*clicks_unit_multiplier).format('0,0'));
+                advertiseTotalCost.parent().removeClass("fadeOutDown");
+                advertiseTotalClicks.parent().removeClass("fadeOutDown delay-150ms");
+                advertiseTotalCost.parent().addClass("fadeInUp delay-150ms");
+                advertiseTotalClicks.parent().addClass("fadeInUp");
+            }, 700);
+        }, 500);
     }
     
     let format_budget_input = function() {
         if (advertiseBudgetInput.val().length < 2) {        
             advertiseBudgetInput.val(selected_denomination);
-            advertiseBudgetSlider.val(1);
+            advertiseBudgetSlider.val(minimum_budget);
             sync_input_slider(true);
         }
     }
@@ -67,10 +83,21 @@ $(() => {
     });
 
     $(document).on("input", "#advertiseBudgetInput", (e) => {
-        console.log(Number.isInteger(parseInt(e.originalEvent.data)));
         if (e.originalEvent.data && !Number.isInteger(parseInt(e.originalEvent.data))) {
             advertiseBudgetInput.val(advertiseBudgetInput.val().substr(0, advertiseBudgetInput.val().indexOf(e.originalEvent.data)));
         }
+        
+        // Enforce maximum and minimum budget selections here
+        if (parseInt(advertiseBudgetInput.val().substr(1)) > maximum_budget) {
+            advertiseBudgetInput.val(selected_denomination+maximum_budget);
+        } else if (parseInt(advertiseBudgetInput.val().substr(1)) < minimum_budget) {
+            advertiseBudgetInput.val(selected_denomination+minimum_budget);
+        }
+
+        if (parseInt(advertiseBudgetInput.val()[1]) === 0) {
+            advertiseBudgetInput.val(advertiseBudgetInput.val().removeCharAt(2));
+        }
+
         format_budget_input();
         sync_input_slider(false);
     });
